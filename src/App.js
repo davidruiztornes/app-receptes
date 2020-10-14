824dbb421529af84eeb83d1190c478864bab841f
David Ruiz Tornés <davidruiz1996@gmail.com>
2020-10-14 19:47:02 +0200

    canvis container app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/heading-has-content */
+import React,{useEffect,useState}from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const App = () =>{
+    const APP_ID = "9fc97f47";
+    const APP_KEY = "8f8e2b7ecbe9313e57983208139119ef";
+
+    const request = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  }
+
+
+  const [counter,setCounter] = useState (0);
+
+  useEffect(()=>{
+    console.log("Efecte Iniciat")
+  },[]);
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form className="search-form">
+       <input className="search-bar" type="text"/>
+       <button className="search-button" type="submit"> Busqueda</button>
+     </form>
+    <h1  onClick={() => setCounter(counter+1)} >{counter}</h1>
     </div>
   );
 }
